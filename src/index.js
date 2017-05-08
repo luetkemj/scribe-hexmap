c717aec7cd0d07389433fa969d4bca7afc28d739
Mark Luetke <luetkemj@gmail.com>
2017-05-08 08:40:09 -0500

    export getHexagons
    missed a function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export keyById from './utils';
 export idMap from './utils';
 export idToHex from './utils';
+export getHexagons from './utils';
 export getRandomHexIdsSample from './utils';
 export getRandomHexIdsShuffle from './utils';
 

f0c0acdb1622ace5ff75c0fcb30294a0f00ab88a
Mark Luetke <luetkemj@gmail.com>
2017-05-08 08:21:35 -0500

    export functions
    add exports to index and remove useless test.
    also bump v

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+export keyById from './utils';
+export idMap from './utils';
+export idToHex from './utils';
+export getRandomHexIdsSample from './utils';
+export getRandomHexIdsShuffle from './utils';
 
+export seedMap from './create-zones';
+export growSeeds from './create-zones';

de905574a34b1c3a03e81b974001926f9f322c90
Mark Luetke <luetkemj@gmail.com>
2017-05-08 08:54:00 -0500

    export properly
    syntax was wrong. should work now

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-export keyById from './utils';
-export idMap from './utils';
-export idToHex from './utils';
-export getHexagons from './utils';
-export getRandomHexIdsSample from './utils';
-export getRandomHexIdsShuffle from './utils';
+export {
+  keyById,
+  idMap,
+  idToHex,
+  getHexagons,
+  getRandomHexIdsSample,
+  getRandomHexIdsShuffle } from './utils';
 
-export seedMap from './create-zones';
-export growSeeds from './create-zones';
+export {
+  seedMap,
+  growSeeds } from './create-zones';
